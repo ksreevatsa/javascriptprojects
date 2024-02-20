3e82b86bbf1861bdd1505c5bd02349b9677ca02a
Sreevatsa_Kota <sreevatsarepalle@gmail.com>
2024-02-20 18:28:14 +0530

    calcualator.js

---
+++ calculator_js/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+public class Main {
+ public static void main(String[]args)
+ {
+    System.out.print(" I want to be the");
+    System.out.print("\"best\"");
+    System.out.print(" coder");
+ }
+}
