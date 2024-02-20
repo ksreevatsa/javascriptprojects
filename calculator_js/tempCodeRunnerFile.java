5cd7e6df685fccf6b1f17026656dfe1561f19189
Sreevatsa_Kota <sreevatsarepalle@gmail.com>
2024-02-20 18:33:30 +0530

    output screen shots added

--- calculator_js/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-public class Main {
- public static void main(String[]args)
- {
-    System.out.print(" I want to be the");
-    System.out.print("\"best\"");
-    System.out.print(" coder");
- }
-}
